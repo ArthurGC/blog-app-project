050dddd3c15cbbb75c8bd7050f5727107bc24ca3
Arthur Guzman <alxguzmanc@gmail.com>
2021-11-07 18:53:40 -0500

    Add recent_comments method to Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,9 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
 end
