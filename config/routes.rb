83685e8ba84578115c473403389c2fd28212413a
Arthur Guzman <alxguzmanc@gmail.com>
2021-11-10 17:18:45 -0500

    Add routes to comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  # get 'posts/index'
-  # get 'posts/show'
-  # get 'users/index'
-  # get 'users/show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "users#index"
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show] do
+      resources :comments, only: [:new, :create]
+    end
   end
-
   resources :posts, only: [:new, :create]
 end
