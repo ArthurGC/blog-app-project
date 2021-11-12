0e807cefc30af7cae37f8adea29f1699a77230c1
Arthur Guzman <alxguzmanc@gmail.com>
2021-11-12 10:05:47 -0500

    Add validations to Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,14 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
+  validates :title, presence: true
+  validates :title, :length => { :maximum => 250,
+    :too_long => "%{count} characters is the maximum allowed" }
+  validates :comments_counter, :numericality => { :only_integer => true,
+    :greater_than_or_equal_to => 0 }
+  validates :likes_counter, :numericality => { :only_integer => true,
+    :greater_than_or_equal_to => 0 }
+
   def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
