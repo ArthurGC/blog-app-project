9d4521d479c59ca8ced8e5005f414fb7e6983c36
arthurgc <alxguzmanc@gmail.com>
2021-11-15 16:08:08 -0500

    env change

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,15 +44,6 @@ Rails.application.configure do
   
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
-  config.action_mailer.smtp_settings = {
-    user_name:      'alexanderdrigo@gmail.com',
-    password:       'lhvzrwfqmsrmrapo',
-    domain:         'localhost:3000',
-    address:       'smtp.gmail.com',
-    port:          '587',
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
