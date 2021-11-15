8165896e7590d8492be367284350277af8208165
Arthur Guzman <alxguzmanc@gmail.com>
2021-11-15 15:52:23 -0500

    Add email confirm with mailer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,8 @@ before_action :update_allowed_parameters, if: :devise_controller?
   protected
 
   def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :photo ,:email, :password) }
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :photo, :email, :password, :current_password) }
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :photo ,:email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :photo, :email, :password, :current_password, :password_confirmation) }
   end
 
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,11 +34,25 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_caching = true
+
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
+  config.action_mailer.smtp_settings = {
+    user_name:      'alexanderdrigo@gmail.com',
+    password:       'lhvzrwfqmsrmrapo',
+    domain:         'localhost:3000',
+    address:       'smtp.gmail.com',
+    port:          '587',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
