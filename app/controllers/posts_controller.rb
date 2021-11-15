200eddb431621fc2f785cf82430cffa233e45da2
Arthur Guzman <alxguzmanc@gmail.com>
2021-11-15 17:06:55 -0500

    Fix linter's issues

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :delete]
-
+  before_action :authenticate_user!, only: %i[new create delete]
 
   def index
     @user = User.find(params[:user_id])
