037706c5aaba39277d91cfbcd0b60fd3b2df7b49
Arthur Guzman <alxguzmanc@gmail.com>
2021-11-15 14:05:26 -0500

    Add user_signed_in to show form only if you logs in

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :delete]
+
+
   def index
     @user = User.find(params[:user_id])
     @post = @user.posts.includes(:comments)
