2e3f3e22da69e892480b104cf09ffa5b8d86a7b8
Arthur Guzman <alxguzmanc@gmail.com>
2021-11-16 10:26:43 -0500

    Add role column to Users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,12 @@ class User < ApplicationRecord
   validates :posts_counter, numericality: { only_integer: true,
                                             greater_than_or_equal_to: 0 }
 
+  Roles = [ :admin , :default ]
+
+  def is?( requested_role )
+    self.role == requested_role.to_s
+  end
+  
   def recent_posts
     posts.order(created_at: :desc).limit(3)
   end
