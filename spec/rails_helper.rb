cfbc208ed1e2c63afa17b41b262af4129774dbe6
Arthur Guzman <alxguzmanc@gmail.com>
2021-11-18 02:26:36 -0500

    Add authorize api request test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,13 @@
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'spec_helper'
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../config/environment', __dir__)
+# Prevent database truncation if the environment is production
+abort('The Rails environment is running in production mode!') if Rails.env.production?
+require 'rspec/rails'
 require 'database_cleaner'
+require 'capybara/rspec'
+# Add additional requires below this line. Rails is not loaded until this point!
 
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
@@ -7,15 +16,6 @@ Shoulda::Matchers.configure do |config|
   end
 end
 
-require 'capybara/rspec'
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'spec_helper'
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../config/environment', __dir__)
-# Prevent database truncation if the environment is production
-abort('The Rails environment is running in production mode!') if Rails.env.production?
-require 'rspec/rails'
-# Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -30,7 +30,7 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -44,8 +44,8 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-    # add `FactoryBot` methods
-    config.include FactoryBot::Syntax::Methods
+  # add `FactoryBot` methods
+  config.include FactoryBot::Syntax::Methods
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
@@ -62,6 +62,9 @@ RSpec.configure do |config|
     end
   end
   # [...]
+
+  config.include RequestSpecHelper
+  config.include ControllerSpecHelper
 
   config.before(:each, js: true) { DatabaseCleaner.strategy = :truncation }
   config.before(:each) { DatabaseCleaner.start }
