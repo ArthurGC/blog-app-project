cdc885207e369779e0e303de18fcf7ddfee1feba
Arthur Guzman <alxguzmanc@gmail.com>
2021-11-18 01:13:00 -0500

    Add routes for api

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,14 @@ Rails.application.routes.draw do
     end
   end
   resources :posts, only: [:new, :create]
+  post 'login', to: 'authentication#authenticate'
+  post 'signup', to: 'users#create'
+    namespace "api", defaults: { format: :json } do
+      resources :users do
+        resources :posts, only: [:index] do
+          resources :comments, only: [:index, :create]
+        end
+      end
+    end
+
 end
