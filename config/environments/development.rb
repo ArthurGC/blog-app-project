bc65fc37dd3416f2cad6ee43447c11fbffd9fa97
Arthur Guzman <alxguzmanc@gmail.com>
2021-11-20 14:32:52 -0500

    Add credentials

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,12 +36,12 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
+  config.action_mailer.default_url_options = { host: Rails.application.credentials.mail_host }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    user_name:      ENV['SENDMAIL_USERNAME'],
-    password:       ENV['SENDMAIL_PASSWORD'],
-    domain:         ENV['MAIL_HOST'],
+    user_name:      Rails.application.credentials.send_email,
+    password:       Rails.application.credentials.send_password,
+    domain:         Rails.application.credentials.mail_host,
     address:       'smtp.gmail.com',
     port:          '587',
     authentication: :plain,
